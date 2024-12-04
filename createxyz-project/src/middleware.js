f7151655442b6b3e4ef254872d1845b22ef168f2
Harumaki587 <harumakigraph@gmail.com>
2024-12-04 23:00:56 +0900

    Add files via upload

---
+++ createxyz-project/src/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/integrations/:path*",
+};
+
+export function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-createxyz-project-id", "2b75981c-f853-4c9c-a0d6-b9e9d882b6e9");
+
+  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
